56cb1c828265113a9b1f89e2cb611204219b5a53
andreybashtovoy <39826086+andreybashtovoy@users.noreply.github.com>
2021-01-02 17:33:16 +0200

    removed old user_stats and other files with old syntax
    added update button functionality with special XML attribute

--- components/group_stats/__init__.py
+++ components/group_stats/__init__.py
@@ -3,7 +3,6 @@ from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler
 from data.ds import Data
 
 
-
 # class UserStats:
 #     def __init__(self, updater):
 #         updater.dispatcher.add_handler(CommandHandler('stats', self.stats))
