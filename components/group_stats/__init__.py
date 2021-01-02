22f48438cf2da8ccce0c040cbd3b02ee7d695c11
andreybashtovoy <39826086+andreybashtovoy@users.noreply.github.com>
2021-01-02 17:31:14 +0200

    test commit

--- components/group_stats/__init__.py
+++ components/group_stats/__init__.py
@@ -3,6 +3,7 @@ from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler
 from data.ds import Data
 
 
+
 # class UserStats:
 #     def __init__(self, updater):
 #         updater.dispatcher.add_handler(CommandHandler('stats', self.stats))
