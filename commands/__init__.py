c14cf26f4b1cd0fac0db9abe9e516cf4e2abae02
andreybashtovoy <39826086+andreybashtovoy@users.noreply.github.com>
2021-01-06 23:04:38 +0200

    get_chat_id

--- commands/__init__.py
+++ commands/__init__.py
@@ -7,8 +7,11 @@ class CommandHandlers:
     def __init__(self, updater: Updater):
         self.__updater = updater
         updater.dispatcher.add_handler(CommandHandler('restart', self.restart))
+        updater.dispatcher.add_handler(CommandHandler('get_chat_id', self.get_chat_id))
 
     def restart(self, update: Update, context):
         update.message.reply_text("До связи")
         os._exit(0)
 
+    def get_chat_id(self, update: Update, context: CallbackContext):
+        update.message.reply_text(update.effective_chat.id)
